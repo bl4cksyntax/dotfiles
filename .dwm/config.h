8fa8ac8ea65cc8fac4df406706bf86da3f39918d
Manas Prakash <bl4ck.syntax@gmail.com>
2021-11-18 23:27:05 +0530

    changed font

--- .dwm/config.h
+++ .dwm/config.h
@@ -13,8 +13,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "UbuntuMono Nerd Font:size=11" };
-static const char dmenufont[]       = "UbuntuMono Nerd Font:size=10";
+static const char *fonts[]          = { "Inconsolata Nerd Font:size=11" };
+static const char dmenufont[]       = "Inconsolata Nerd Font:size=10";
 static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%",     NULL };
 static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%",     NULL };
 static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "@DEFAULT_SINK@", "toggle",  NULL };
